9f925677ae57f16d2cfaa24dce771d46f18d9f4f
cvs2svn <admin@example.com>
1997-07-22 10:00:23 +0000

    This commit was manufactured by cvs2git to create tag 'startup100'.

--- src/etc/root/dot.profile
+++
@@ -1,9 +0,0 @@
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
-export PATH
-HOME=/root
-export HOME
-umask 022
-
-if [ -x /usr/bin/tset ]; then
-	eval `/usr/bin/tset -sQ \?$TERM`
-fi
--- src/include/poll.h
+++
@@ -1,7 +0,0 @@
-/*	$OpenBSD: poll.h,v 1.1 1996/05/18 08:53:07 deraadt Exp $ */
-
-/*
- * Typical poll() implimentations expect poll.h to be in /usr/include. 
- * However this is not a convenient place for the real definitions.
- */
-#include <sys/poll.h>
