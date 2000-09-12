e585285108a7232b6ab34130bf22ce874d27bf80
millert <>
2000-09-12 17:30:45 +0000

    o add missing getsid() prototype o fix typo in getsid() that broke getsid(pid) where pid != 0 o cause getsid() and getpgid() to return EPERM if requesting the   id of a session/process group not in the current session o check for NULL session in getsid() for exiting processes

--- src/include/unistd.h
+++ src/include/unistd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: unistd.h,v 1.31 2000/04/20 06:34:18 deraadt Exp $ */
+/*	$OpenBSD: unistd.h,v 1.32 2000/07/19 19:26:04 mickey Exp $ */
 /*	$NetBSD: unistd.h,v 1.26.4.1 1996/05/28 02:31:51 mrg Exp $	*/
 
 /*-
@@ -85,6 +85,7 @@ pid_t	 getpgrp __P((void));
 pid_t	 getpid __P((void));
 pid_t	 getpgid __P((pid_t));
 pid_t	 getppid __P((void));
+pid_t	 getsid __P((pid_t));
 uid_t	 getuid __P((void));
 int	 isatty __P((int));
 int	 link __P((const char *, const char *));
