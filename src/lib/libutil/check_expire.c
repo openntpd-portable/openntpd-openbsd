79587e0a0d0647386e288a022ec694c41f1cc281
millert <>
2001-08-16 18:39:20 +0000

    2nd arg to pw_mkdb()

--- src/lib/libutil/check_expire.c
+++ src/lib/libutil/check_expire.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: check_expire.c,v 1.1 2000/11/26 01:27:19 millert Exp $	*/
+/*	$OpenBSD: check_expire.c,v 1.2 2001/08/16 18:34:40 millert Exp $	*/
 
 /*
  * Copyright (c) 1997 Berkeley Software Design, Inc. All rights reserved.
@@ -182,7 +182,7 @@ pwd_update(pwd)
 	}
 
 	pw_copy(pfd, tfd, pwd);
-	if (pw_mkdb(pwd->pw_name) < 0) {
+	if (pw_mkdb(pwd->pw_name, 0) < 0) {
 		pw_abort();
 		return("unable to update password database");
 	}
