505d6c15084b83c4dade3d0f3d78a128e2090fec
henning <>
2004-07-08 01:22:57 +0000

    remove all handling of netmasks/prefix lengths - we don't need that in ntpd. fixes the dns resolves to v4 and v6 addresses bug found by phessler hacked on the Calgary->Montreal flight that proved that Air Canada _does_ have some modern aircrafts with good seats

--- src/usr.sbin/ntpd/ntpd.h
+++ src/usr.sbin/ntpd/ntpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ntpd.h,v 1.16 2004/07/07 05:47:57 henning Exp $ */
+/*	$OpenBSD: ntpd.h,v 1.17 2004/07/07 07:32:05 alexander Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -184,7 +184,7 @@ int	 cmdline_symset(char *);
 
 /* config.c */
 int		 check_file_secrecy(int, const char *);
-struct ntp_addr	*host(const char *, u_int8_t *);
+struct ntp_addr	*host(const char *);
 
 /* ntp_msg.c */
 int	ntp_getmsg(char *, ssize_t, struct ntp_msg *);
