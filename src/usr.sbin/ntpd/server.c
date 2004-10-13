494890f2fe9dfb4fe202188c77c3a00de981b445
henning <>
2004-10-13 12:22:39 +0000

    correctly set refid in replies with NTP protocol versions < 4. code path for NTP4 remains unchanged, we already set refid correctly there. NTP3 and older uses an IPv4 address as refid. use the IP of the server we last synced to if it was a IPv4 one. sometimes we use the average offset between two, in that case just pick one for the IP. this scheme naturally fails when we query IPv6 servers and have to reply to IPv4 NTP3 (or even older NTP versions) clients - refid stays at 0 then. this is a protocol limitation, nothing we can do about it.

--- src/usr.sbin/ntpd/server.c
+++ src/usr.sbin/ntpd/server.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: server.c,v 1.12 2004/07/18 13:26:53 henning Exp $ */
+/*	$OpenBSD: server.c,v 1.13 2004/09/07 22:43:07 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -140,7 +140,7 @@ server_dispatch(int fd, struct ntpd_conf *conf)
 	if (version > 3)
 		reply.refid = reply.xmttime.fraction;
 	else
-		reply.refid = 0;	/* XXX */
+		reply.refid = conf->status.refid;
 
 	ntp_sendmsg(fd, (struct sockaddr *)&fsa, &reply, size, 0);
 	return (0);
