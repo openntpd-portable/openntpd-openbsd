a587c6a3419aef69f680e5dfa01fc1d3bba1cd34
espie <>
2005-05-11 18:39:19 +0000

    let vfscanf be a real function. Use a weak_alias on systems where this is feasible.
    Okay millert@
    
    There's a major libc bump coming that is going to cover this as well...

--- src/include/stdio.h
+++ src/include/stdio.h
@@ -327,14 +327,10 @@ int	 vscanf(const char *, _BSD_VA_LIST_)
 int	 vsscanf(const char *, const char *, _BSD_VA_LIST_)
 		__attribute__((__format__ (scanf, 2, 0)))
 		__attribute__((__nonnull__ (2)));
+int	 vfscanf(FILE *, const char *, _BSD_VA_LIST_)
+		__attribute__((__format__ (scanf, 2, 0)))
+		__attribute__((__nonnull__ (2)));
 __END_DECLS
-
-/*
- * This is a #define because the function is used internally and
- * (unlike vfscanf) the name __svfscanf is guaranteed not to collide
- * with a user function when _ANSI_SOURCE or _POSIX_SOURCE is defined.
- */
-#define	 vfscanf	__svfscanf
 
 /*
  * Stdio function-access interface.
@@ -355,7 +351,6 @@ __END_DECLS
  */
 __BEGIN_DECLS
 int	__srget(FILE *);
-int	__svfscanf(FILE *, const char *, _BSD_VA_LIST_);
 int	__swbuf(int, FILE *);
 __END_DECLS
 
