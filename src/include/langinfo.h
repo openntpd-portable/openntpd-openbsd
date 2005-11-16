3feab1906812b43ace69be4d75413ed445ea2301
espie <>
2005-11-16 16:48:14 +0000

    CODESET support. okay millert@, otto@
    (not really useful for now, it mostly helps some programs which want
    to use it, it will become useful when we have full 16 bits locale).

--- src/include/langinfo.h
+++ src/include/langinfo.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: langinfo.h,v 1.3 2001/01/29 01:32:37 naddy Exp $	*/
+/*	$OpenBSD: langinfo.h,v 1.4 2002/02/16 21:27:17 millert Exp $	*/
 /*	$NetBSD: langinfo.h,v 1.3 1995/04/28 23:30:54 jtc Exp $	*/
 
 /*
@@ -68,6 +68,8 @@
 #define NOSTR		48	/* Negative response for yes/no queries */
 #define NOEXPR		49	/* Negative response for yes/no queries */
 #define CRNCYSTR	50	/* Currency symbol */
+
+#define CODESET		51	/* Codeset name */
 
 __BEGIN_DECLS
 char *nl_langinfo(nl_item);
