4a5a9ead5dc1fdbb0768a266eecb7c1135f65844
otto <>
2007-11-22 10:24:25 +0000

    be a bit less aggressive retrying; this keeps the message queue empty while in the -s period, so the poll timeout actually times out if there are no interfaces available. ok henning@

--- src/usr.sbin/ntpd/client.c
+++ src/usr.sbin/ntpd/client.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: client.c,v 1.75 2007/04/30 01:33:33 deraadt Exp $ */
+/*	$OpenBSD: client.c,v 1.76 2007/05/01 07:40:45 otto Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -123,7 +123,8 @@ client_query(struct ntp_peer *p)
 	int	tos = IPTOS_LOWDELAY;
 
 	if (p->addr == NULL && client_nextaddr(p) == -1) {
-		set_next(p, scale_interval(INTERVAL_QUERY_AGGRESSIVE));
+		set_next(p, MAX(SETTIME_TIMEOUT,
+		    scale_interval(INTERVAL_QUERY_AGGRESSIVE)));
 		return (0);
 	}
 
@@ -140,8 +141,8 @@ client_query(struct ntp_peer *p)
 			if (errno == ECONNREFUSED || errno == ENETUNREACH ||
 			    errno == EHOSTUNREACH || errno == EADDRNOTAVAIL) {
 				client_nextaddr(p);
-				set_next(p,
-				    scale_interval(INTERVAL_QUERY_AGGRESSIVE));
+				set_next(p, MAX(SETTIME_TIMEOUT,
+				    scale_interval(INTERVAL_QUERY_AGGRESSIVE)));
 				return (-1);
 			} else
 				fatal("client_query connect");
