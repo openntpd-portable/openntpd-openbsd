c55202eca302cf4a4ea458893d6c6a9e07ff22c9
otto <>
2008-06-24 06:01:33 +0000

    remove recalloc; sloppy me; ok deraadt@

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: stdlib.h,v 1.42 2008/06/13 21:04:24 landry Exp $	*/
+/*	$OpenBSD: stdlib.h,v 1.43 2008/06/20 15:21:06 ragge Exp $	*/
 /*	$NetBSD: stdlib.h,v 1.25 1995/12/27 21:19:08 jtc Exp $	*/
 
 /*-
@@ -128,7 +128,6 @@ void	*malloc(size_t);
 void	 qsort(void *, size_t, size_t, int (*)(const void *, const void *));
 int	 rand(void);
 void	*realloc(void *, size_t);
-void	*recalloc(void *, size_t, size_t);
 void	 srand(unsigned);
 double	 strtod(const char *, char **);
 float	 strtof(const char *, char **);
