6f644f3a11edd9a5cd5d94614d5b20bf956bd81e
jsg <>
2009-09-22 21:40:02 +0000

    Use __builtin_offsetof for offsetof definition on gcc >= 4 This is required as gcc grows ever more pedantic with old age and cavorts with standards bodies that like to create paradoxes.
    ok kettenis@ miod@ millert@ espie@

--- src/include/stddef.h
+++ src/include/stddef.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: stddef.h,v 1.8 2005/05/11 18:44:12 espie Exp $	*/
+/*	$OpenBSD: stddef.h,v 1.9 2006/01/06 18:53:04 millert Exp $	*/
 /*	$NetBSD: stddef.h,v 1.4 1994/10/26 00:56:26 cgd Exp $	*/
 
 /*-
@@ -72,6 +72,10 @@ typedef	__mbstate_t	mbstate_t;
 #endif
 #endif
 
+#if __GNUC_PREREQ__(4, 0)
+#define	offsetof(type, member)	__builtin_offsetof(type, member)
+#else
 #define	offsetof(type, member)	((size_t)(&((type *)0)->member))
+#endif
 
 #endif /* _STDDEF_H_ */
