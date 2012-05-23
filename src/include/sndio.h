b363da656739a93b140c4dbb597f81a7eb8f9f48
ratchov <>
2012-05-23 19:25:11 +0000

    Make the "default" string a valid device name that has the same effect as NULL has. This will (hopefully) simplify ports where the user passes the device string.

--- src/include/sndio.h
+++ src/include/sndio.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sndio.h,v 1.4 2010/11/06 20:25:42 ratchov Exp $	*/
+/*	$OpenBSD: sndio.h,v 1.5 2012/04/11 06:05:43 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -18,6 +18,12 @@
 #define SNDIO_H
 
 #include <sys/param.h>
+
+/*
+ * default audio device and MIDI port
+ */
+#define SIO_DEVANY	"default"
+#define MIO_PORTANY	"default"
 
 /*
  * private ``handle'' structure
