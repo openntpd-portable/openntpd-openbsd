b31e60551e0a28d490796a1898a2128a7f59f238
espie <>
2012-08-22 08:56:46 +0000

    hex constants unsigned, prevents some gcc warnings okay ratchov@

--- src/include/sndio.h
+++ src/include/sndio.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sndio.h,v 1.6 2012/05/23 19:25:11 ratchov Exp $	*/
+/*	$OpenBSD: sndio.h,v 1.7 2012/07/08 17:55:50 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -51,7 +51,7 @@ struct sio_par {
 	unsigned int round;	/* optimal bufsz divisor */
 	unsigned int appbufsz;	/* minimum buffer size */
 	int __pad[3];		/* for future use */
-	int __magic;		/* for internal/debug purposes only */
+	unsigned int __magic;	/* for internal/debug purposes only */
 };
 
 /*
