5ac03ca920c3dad6b9f3ec53b79f86c5aa74a877
guenther <>
2012-11-29 02:15:44 +0000

    Change scandir()'s 'select' argument from 	int (*)(struct dirent *) to 	int (*)(const struct dirent *) to match POSIX.
    ok millert@, ports check by naddy@

--- src/include/dirent.h
+++ src/include/dirent.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dirent.h,v 1.27 2012/03/22 04:11:53 matthew Exp $	*/
+/*	$OpenBSD: dirent.h,v 1.28 2012/03/24 01:53:51 guenther Exp $	*/
 /*	$NetBSD: dirent.h,v 1.9 1995/03/26 20:13:37 jtc Exp $	*/
 
 /*-
@@ -95,7 +95,7 @@ int readdir_r(DIR *__restrict, struct dirent *__restrict,
     struct dirent **__restrict);
 #endif
 #if __POSIX_VISIBLE >= 200809
-int scandir(const char *, struct dirent ***, int (*)(struct dirent *),
+int scandir(const char *, struct dirent ***, int (*)(const struct dirent *),
     int (*)(const struct dirent **, const struct dirent **));
 int alphasort(const struct dirent **, const struct dirent **);
 #elif __BSD_VISIBLE
