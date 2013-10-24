74977dbb8423adcdcf84bb8c499b91ddf1ec6430
guenther <>
2013-10-24 07:34:56 +0000

    Align suseconds_t with POSIX: it's the type of the tv_usec member of timeval. Use useconds_t in the ualarm() declaration. Bump libstdc++ major to be sure there isn't ABI issues.
    ok deraadt@ jca@ jmc@ millert@
    ports testing by landry@

--- src/include/unistd.h
+++ src/include/unistd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: unistd.h,v 1.78 2013/06/04 23:01:07 brad Exp $ */
+/*	$OpenBSD: unistd.h,v 1.79 2013/06/17 19:11:54 guenther Exp $ */
 /*	$NetBSD: unistd.h,v 1.26.4.1 1996/05/28 02:31:51 mrg Exp $	*/
 
 /*-
@@ -428,7 +428,7 @@ int	 setreuid(uid_t, uid_t);
 void	 swab(const void *, void *, size_t);
 void	 sync(void);
 int	 truncate(const char *, off_t);
-unsigned int	 ualarm(unsigned int, unsigned int);
+useconds_t	 ualarm(useconds_t, useconds_t);
 int	 usleep(useconds_t);
 pid_t	 vfork(void);
 #endif
