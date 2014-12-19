efaf711428b60c4758055d598bfd03bc94fd855f
deraadt <>
2014-12-19 14:30:44 +0000

    i386 unrolling blows up the media in a big way, due to -Os for ramdisk libc builds.  there has to be a better way without #ifdef's in gross places, but I don't see it yet.

--- src/lib/libc/hash/sha2.c
+++ src/lib/libc/hash/sha2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sha2.c,v 1.19 2014/12/17 19:42:44 tedu Exp $	*/
+/*	$OpenBSD: sha2.c,v 1.20 2014/12/18 20:29:08 tedu Exp $	*/
 
 /*
  * FILE:	sha2.c
@@ -52,7 +52,7 @@
  *   #define SHA2_UNROLL_TRANSFORM
  *
  */
-#if defined(__amd64__) || defined(__i386__)
+#if defined(__amd64__)
 #define SHA2_UNROLL_TRANSFORM
 #endif
 
